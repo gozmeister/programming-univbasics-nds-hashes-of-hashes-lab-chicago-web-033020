9ba558d5b8c1c9def234a613ddc153d7b4a9f715
Sam Gorick <samgorick.sg@gmail.com>
2020-03-11 19:01:38 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,8 +32,25 @@
 
 def naming_system
   {
-    PORTION_1
-  }
+   label: "Kingdom",
+   sub_category: {
+     label: "Phylum",
+     sub_category: {
+       label: "Class",
+       sub_category: {
+         label: "Order",
+         sub_category: {
+           label: "Family",
+           sub_category: {
+             label: "Genus",
+             sub_category: {
+               label: "Species"
+             }
+           }
+         }
+       }
+     }
+   }
   # Remember:
   #  Kingdom
   #  Phylum
